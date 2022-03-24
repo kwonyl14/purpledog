346f19573e13726cef6c4660160827a0baac8230
sqk8657 <sqk8657@naver.com>
2022-03-24 18:29:59 +0900

    Fix: 유저 엔티티 주석 추가

--- src/main/java/com/purpledog/younglin/user/entity/User.java
+++ src/main/java/com/purpledog/younglin/user/entity/User.java
@@ -3,6 +3,11 @@ package com.purpledog.younglin.user.entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+/**
+ * @FileName : User
+ * @Class 설명 : DB의 회원 테이블과 맵핑될 User 엔티티
+ */
+
 @Entity
 public class User {
 
