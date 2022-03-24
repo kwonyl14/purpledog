4721a59b1ad6d0ce919a2b64682d764e6b126c61
sqk8657 <sqk8657@naver.com>
2022-03-24 18:12:56 +0900

    Fix: 유저 레포지토리 주석 추가

--- src/main/java/com/purpledog/younglin/user/repository/UserRepository.java
+++ src/main/java/com/purpledog/younglin/user/repository/UserRepository.java
@@ -4,6 +4,11 @@ import com.purpledog.younglin.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @FileName : UserRepository
+ * @Class 설명 : 유저 DB에 접근할 유저 JPA 레포지토리
+ */
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 }
