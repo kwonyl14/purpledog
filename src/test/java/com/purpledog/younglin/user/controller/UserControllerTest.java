0006cf0f3d5e46b4d19e5a05a611eb7f3636ff06
sqk8657 <sqk8657@naver.com>
2022-03-25 13:35:52 +0900

    Feat: 회원 비밀번호 수정 컨트롤러 테스트

--- src/test/java/com/purpledog/younglin/user/controller/UserControllerTest.java
+++ src/test/java/com/purpledog/younglin/user/controller/UserControllerTest.java
@@ -2,7 +2,9 @@ package com.purpledog.younglin.user.controller;
 
 import com.google.gson.Gson;
 import com.purpledog.younglin.user.dto.request.UserCreateReq;
+import com.purpledog.younglin.user.dto.request.UserUpdateReq;
 import com.purpledog.younglin.user.dto.response.UserCreateRes;
+import com.purpledog.younglin.user.dto.response.UserUpdateRes;
 import com.purpledog.younglin.user.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,9 +19,15 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @FileName : UserControllerTest
+ * @Class 설명 : 유저 컨트롤러 기능 단위 테스트
+ */
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(UserController.class)
@@ -41,13 +49,56 @@ class UserControllerTest {
         //json형태로 변환
         Gson gson = new Gson();
         String content = gson.toJson(userCreateReq);
-        String reponse = gson.toJson(userCreateRes);
+        String response = gson.toJson(userCreateRes);
 
         //when, then
         mockMvc.perform(post("/api/user")
                         .content(content)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().json(reponse));
+                .andExpect(content().json(response));
+    }
+
+    @Test
+    @DisplayName("회원 비밀번호 변경 테스트")
+    void changeUserPassword() throws Exception {
+        //given
+        String id = "id";
+        String updatedPassword = "updatedPassword";
+        UserUpdateReq userUpdateReq = new UserUpdateReq(id, updatedPassword);
+        UserUpdateRes userUpdateRes = UserUpdateRes.of(userUpdateReq.toEntity());
+        given(userService.updateUserPassword(any(UserUpdateReq.class))).willReturn(userUpdateRes);
+        //json형태로 변환
+        Gson gson = new Gson();
+        String content = gson.toJson(userUpdateReq);
+        String response = gson.toJson(userUpdateRes);
+
+        //when, then
+        mockMvc.perform(patch("/api/user")
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(response));
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 회원 비밀번호 변경 테스트")
+    void changeNotExistUserPassword() throws Exception {
+        //given
+        String id = "notExistId";
+        String updatedPassword = "updatedPassword";
+        UserUpdateReq userUpdateReq = new UserUpdateReq(id, updatedPassword);
+        given(userService.updateUserPassword(any(UserUpdateReq.class))).willReturn(null);
+        //json형태로 변환
+        Gson gson = new Gson();
+        String content = gson.toJson(userUpdateReq);
+        String response = "";
+
+        //when, then
+        mockMvc.perform(patch("/api/user")
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andExpect(content().string(response));
     }
 }
