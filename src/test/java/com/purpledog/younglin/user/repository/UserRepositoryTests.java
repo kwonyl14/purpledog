8f78a243a344598db273c74c033e1e3fafbc99f6
sqk8657 <sqk8657@naver.com>
2022-03-25 13:34:41 +0900

    Feat: 회원 비밀번호 수정 레포지토리 테스트

--- src/test/java/com/purpledog/younglin/user/repository/UserRepositoryTests.java
+++ src/test/java/com/purpledog/younglin/user/repository/UserRepositoryTests.java
@@ -8,9 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
 /**
  * @FileName : UserRepositoryTests
- * @Class 설명 : 유저 레포지토리 테스트
+ * @Class 설명 : 유저 레포지토리 기능 단위 테스트
  */
 
 @DataJpaTest
@@ -27,8 +31,41 @@ public class UserRepositoryTests {
     void insertUser() {
         User user = new User("id", "password");
         User saveUser = userRepository.save(user);
-        Assertions.assertThat(saveUser.getId()).isEqualTo("id");
-        Assertions.assertThat(saveUser.getPassword()).isEqualTo("password");
+        assertThat(saveUser.getId()).isEqualTo("id");
+        assertThat(saveUser.getPassword()).isEqualTo("password");
     }
+
+    @Test
+    @DisplayName("유저 비밀번호 변경 테스트")
+    void updateUserPassword() {
+        //given
+        User user = new User("id", "password");
+        User saveUser = userRepository.save(user);
+
+        //when
+        saveUser.setPassword("UpdatedPassword");
+
+        //then
+        User userById = userRepository.findById("id").orElse(null);
+        assertThat(userById).isNotNull();
+        assertThat(userById.getPassword()).isNotEqualTo("password");
+        assertThat(userById.getPassword()).isEqualTo("UpdatedPassword");
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 유저 비밀번호 변경 테스트")
+    void updateNotExistUserPassword() {
+        //given
+        User user = new User("id", "password");
+        User saveUser = userRepository.save(user);
+
+        //when
+        saveUser.setPassword("UpdatedPassword");
+
+        //then
+        User userById = userRepository.findById("id3").orElse(null);
+        assertThat(userById).isNull();
+    }
+
 
 }
